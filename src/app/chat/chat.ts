560c1a3b3d6f470cac6b452d4ba9b22b0d5c0b8c
ByJelias05 <garciassierra@hotmail.com>
2025-06-15 19:34:19 -0400

    Chat: Obtiene el mensaje a enviar

--- src/app/chat/chat.ts
+++ src/app/chat/chat.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule,],
   templateUrl: './chat.html',
   styleUrl: './chat.css'
 })
 export class Chat {
 
+  public Chat: string[] = []
+
+  public Mensaje = new FormControl("")
+
+  public EnviarMensaje(){
+    this.Chat.push(this.Mensaje.value ?? "");
+    
+   
+  }
 }
- 
+  
