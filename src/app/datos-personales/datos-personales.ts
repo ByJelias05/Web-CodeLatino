5a58c98a83a30b0828a3da805c26e72057ad65a5
ByJelias05 <garciassierra@hotmail.com>
2025-06-15 13:55:27 -0400

    Datos personales: Recolecta los Datos + Animaciones

--- src/app/datos-personales/datos-personales.ts
+++ src/app/datos-personales/datos-personales.ts
@@ -1,11 +1,28 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-datos-personales',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './datos-personales.html',
   styleUrl: './datos-personales.css'
 })
 export class DatosPersonales {
+  
+  public CampoSelecionado?:string
+ 
+  public CambiarCampo(campo:string){
+    this.CampoSelecionado = campo
+    console.log(this.Formnulario.value)
+  }
+
+
+  public Formnulario = new FormGroup({
+    Usuario: new FormControl("", Validators.required),
+    Correo: new FormControl("", Validators.required),
+    Contrasena: new FormControl("", Validators.required),
+    RepetirContrasena: new FormControl("", Validators.required),
+  })
 
 }
