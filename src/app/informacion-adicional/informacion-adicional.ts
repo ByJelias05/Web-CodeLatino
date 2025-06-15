05bbb46c81229252c0a3ba3ec2a8c513ab462907
ByJelias05 <garciassierra@hotmail.com>
2025-06-15 18:51:22 -0400

    Info Adicional: Recolecta los datos

--- src/app/informacion-adicional/informacion-adicional.ts
+++ src/app/informacion-adicional/informacion-adicional.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgControl, ReactiveFormsModule, FormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-informacion-adicional',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule,],
   templateUrl: './informacion-adicional.html',
   styleUrl: './informacion-adicional.css'
 })
 export class InformacionAdicional {
 
+  public Region = new FormControl("")
+  public Link = new FormControl("")
+
+  public image= ""
+
+  public ExtaerImagen(event:Event){
+    const input = event.target as HTMLInputElement;
+    if(input.files && input.files.length > 0){
+      this.image = window.URL.createObjectURL(input.files[0])
+    }
+  }
+   
 }
+ 
